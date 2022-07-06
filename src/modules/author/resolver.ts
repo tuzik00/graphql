22169ac12fa10359008e28f7744162a9da81561a
Demidov Evgeniy <demidov.e@citilink.ru>
2022-07-06 14:32:05 +0300

    настройка сборки

--- src/modules/author/resolver.ts
+++ src/modules/author/resolver.ts
@@ -1,5 +1,25 @@
+import { PubSub } from 'graphql-subscriptions';
+
+const pubsub = new PubSub();
+let currentNumber = 0;
+
+function incrementNumber() {
+  currentNumber++;
+  pubsub.publish('NUMBER_INCREMENTED', { numberIncremented: currentNumber });
+  setTimeout(incrementNumber, 1000);
+}
+
+incrementNumber();
+
 export default {
   Query: {
-    hello: () => 'world',
+    currentNumber() {
+      return currentNumber;
+    },
+  },
+  Subscription: {
+    numberIncremented: {
+      subscribe: () => pubsub.asyncIterator(['NUMBER_INCREMENTED']),
+    },
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,52 @@
-import { createServer } from '@graphql-yoga/node';
-import { buildSubgraphSchema } from '@apollo/subgraph';
+import express from 'express';
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import cors from 'cors';
 
 export default async (modules) => {
-  const server = createServer({
-    port: parseInt(process.env.PORT),
-    hostname: process.env.HOST_NAME,
-    schema: buildSubgraphSchema({
-      typeDefs: mergeTypeDefs(modules.map((module) => module.typeDefs)),
-      resolvers: mergeResolvers(
-        modules.map((module) => module.resolvers),
-      ) as never,
-    }),
+  const schema = makeExecutableSchema({
+    typeDefs: mergeTypeDefs(modules.map((module) => module.typeDefs)),
+    resolvers: mergeResolvers(
+      modules.map((module) => module.resolvers),
+    ) as never,
   });
 
-  await server.start();
+  const app = express();
+  app.use(cors());
+
+  const httpServer = createServer(app);
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql',
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  const apolloServer = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+  httpServer.listen(process.env.PORT);
 };
